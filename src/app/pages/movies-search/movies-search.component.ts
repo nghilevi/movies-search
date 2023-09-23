aee3534b47af7369aea77d59611426de2fd955b9
Nghi Le <vinhnghi223@gmail.com>
2023-09-23 10:38:37 +0300

    added caching

--- src/app/pages/movies-search/movies-search.component.ts
+++ src/app/pages/movies-search/movies-search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { BehaviorSubject, finalize, takeUntil, tap } from 'rxjs';
+import { BehaviorSubject, finalize, map, shareReplay, takeUntil, tap } from 'rxjs';
 import { MovieCardsComponent } from 'src/app/components/movie-cards/movie-cards.component';
 import { MovieListItem } from 'src/app/shared/movies.model';
 import { PaginatedResult } from 'src/app/shared/shared.model';
@@ -19,45 +19,21 @@ import { SearchService } from 'src/app/services/search.service';
 })
 export class MoviesSearchComponent extends Unsub implements OnInit {
 
-  private moviesSub = new BehaviorSubject<MovieListItem[]>([]);
-  movies$ = this.moviesSub.asObservable()
-
-  isLoading = false
-  currentPage = 1
+  movies$ = this.searchService.loadedMovies$.pipe(takeUntil(this.unsubscribe$));
   windowScrolled = false;
 
-  constructor(private MoviesApiService: MoviesApiService, private searchService: SearchService) {
+  constructor(public searchService: SearchService) {
     super();
   }
 
   ngOnInit(): void {
-
-    this.searchService.onSearch$
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe((searchValue: string) => {
-      this.currentPage = 1
-      this.loadMovies()
-    });
-
     window.addEventListener('scroll', () => {
       this.windowScrolled = window.pageYOffset !== 0;
     });
   }
 
   loadMoviesMore(){
-    this.currentPage++
-    this.loadMovies()
-  }
-
-  loadMovies(){
-    this.isLoading = true
-    const isLoadMore = this.currentPage > 1
-    const searchValue = this.searchService.searchValue
-    const updateMoviesList = (data: PaginatedResult<MovieListItem>) => {
-      this.moviesSub.next((isLoadMore ? this.moviesSub.value : []).concat(data.results)) // this update movies$
-    }
-    const movies$ = searchValue ? this.MoviesApiService.searchMovies(searchValue, this.currentPage) : this.MoviesApiService.getPopularMovies(this.currentPage)
-    return movies$.pipe(takeUntil(this.unsubscribe$), tap(updateMoviesList), finalize(() => {this.isLoading = false})).subscribe();
+    this.searchService.loadMoreMovies()
   }
 
   scrollToTop(): void {
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,22 +1,66 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, debounceTime, distinctUntilChanged, map, tap } from 'rxjs';
+import { BehaviorSubject, Observable, debounceTime, distinctUntilChanged, finalize, map, shareReplay, switchMap, takeUntil, tap } from 'rxjs';
+import { MovieListItem } from '../shared/movies.model';
+import { PaginatedResult } from '../shared/shared.model';
+import { MoviesApiService } from './movies.api-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
+  isLoading = false;
+  currentPage = 1
+
   private onSearchSub = new BehaviorSubject<string>('');
-  onSearch$ = this.onSearchSub.asObservable().pipe(
+  private onLoadMoreSub = new BehaviorSubject<void>(undefined);
+  private onUpdateMoviesSub = new BehaviorSubject<string>('');
+  loadedMovies: MovieListItem[] = []
+  
+  onLoadMoreMovies$ = this.onLoadMoreSub.pipe(
+    switchMap(() => this.loadMovies()),
+    shareReplay(1) // if there is new subscriber, replay the previous emit
+  )
+
+  onSearchMovies$ = this.onSearchSub.pipe(
     debounceTime(500),
-    distinctUntilChanged()
+    distinctUntilChanged(),
+    tap(() => { this.currentPage = 1 }),
+    switchMap(() => this.loadMovies()),
+    shareReplay(1)
+  )
+
+  loadedMovies$: Observable<MovieListItem[]> = this.onUpdateMoviesSub.pipe(
+    switchMap((searchStr) => searchStr ? this.onSearchMovies$ : this.onLoadMoreMovies$),
   )
 
   search(searchStr: string){
     this.onSearchSub.next(searchStr)
+    this.onUpdateMoviesSub.next(searchStr)
+  }
+
+  loadMoreMovies(){
+    this.currentPage++;
+    this.onLoadMoreSub.next()
+    this.onUpdateMoviesSub.next('')
   }
 
   get searchValue(){
     return this.onSearchSub.value
   }
+
+  constructor(private moviesApiService: MoviesApiService) {}
+
+  loadMovies(): Observable<MovieListItem[]>{
+    this.isLoading = true
+    const searchValue = this.searchValue
+    const updateMoviesList = (data: PaginatedResult<MovieListItem>) => {
+      const isLoadMore = this.currentPage > 1
+      this.loadedMovies = isLoadMore ? this.loadedMovies.concat(data.results) : data.results
+      return this.loadedMovies
+    }
+    const movies$ = searchValue ? this.moviesApiService.searchMovies(searchValue, this.currentPage) : this.moviesApiService.getPopularMovies(this.currentPage)
+    return movies$.pipe(map(updateMoviesList), finalize(() => {this.isLoading = false}));
+  }
+
 }
