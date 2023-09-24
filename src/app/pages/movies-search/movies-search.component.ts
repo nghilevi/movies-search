007e77a921529e92640fdd448abaaa9736a79f97
Nghi Le <vinhnghi223@gmail.com>
2023-09-24 03:03:48 +0300

    added debounce to load more btn

--- src/app/pages/movies-search/movies-search.component.ts
+++ src/app/pages/movies-search/movies-search.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MovieCardComponent } from '../../components/movie-card/movie-card.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { takeUntil } from 'rxjs';
+import { debounceTime, fromEvent, takeUntil, tap } from 'rxjs';
 import { MovieCardsComponent } from 'src/app/components/movie-cards/movie-cards.component';
 import { Unsub } from 'src/app/shared/unsub.class';
 import { MoviesService } from 'src/app/services/movies.service';
@@ -14,23 +14,27 @@ import { MoviesService } from 'src/app/services/movies.service';
   standalone: true,
   imports: [RouterModule, CommonModule, MovieCardComponent, MovieCardsComponent],
 })
-export class MoviesSearchComponent extends Unsub implements OnInit {
-
+export class MoviesSearchComponent extends Unsub implements OnInit, AfterViewInit {
+  
+  @ViewChild('loadMore') loadMoreBtn: ElementRef | undefined;
   movies$ = this.moviesService.movies$.pipe(takeUntil(this.unsubscribe$));
   windowScrolled = false;
 
   constructor(public moviesService: MoviesService) {
     super();
   }
+  ngAfterViewInit(): void {
+    fromEvent(this.loadMoreBtn?.nativeElement, 'click').pipe(
+      debounceTime(250),
+      tap(() => { this.moviesService.loadMoreMovies() }),
+      takeUntil(this.unsubscribe$)
+    ).subscribe();
+  }
 
   ngOnInit(): void {
     window.addEventListener('scroll', () => {
       this.windowScrolled = window.pageYOffset > 200;
     });
-  }
-
-  loadMoviesMore(){
-    this.moviesService.loadMoreMovies()
   }
 
   scrollToTop(): void {
