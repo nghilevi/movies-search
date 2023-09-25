15d2726cf34e7c1fcfe2440f7002b8a92015429d
Nghi Le <vinhnghi223@gmail.com>
2023-09-25 03:52:36 +0300

    cleaned up

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, finalize, map,  of, shareReplay, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, Observable, finalize, map,  of, shareReplay, switchMap, tap } from 'rxjs';
 import { MovieListItem } from '../shared/movies.model';
 import { PaginatedResult } from '../shared/shared.model';
 import { MoviesApiService } from './movies.api-service';
 
 enum UserInteraction {
-  Search = 'search', LoadMore = 'load more'
+  Init = 'init', Search = 'search', LoadMore = 'load more'
 }
 @Injectable({
   providedIn: 'root'
@@ -13,16 +13,9 @@ enum UserInteraction {
 export class MoviesService {
 
   constructor(private moviesApiService: MoviesApiService) {}
-  
-  ///
-  private currentPage = 1
-  private loadedMovies: MovieListItem[] = []
-  private querySub = new BehaviorSubject<string>('');
-  private getMovies$ = this.querySub.pipe(switchMap((query) => this.getMovies(query)), shareReplay(1))
-  ///
 
   private searchedMoviesPage = 1
-  popularMoviesPage = 0
+  popularMoviesPage = 1
 
   private isLoading = false;
   private queryString = ''
@@ -32,23 +25,21 @@ export class MoviesService {
   private popularMoviesQuerySub = new BehaviorSubject<string>('');
 
   private searchedMovies$ = this.searchedMoviesQuerySub.pipe(switchMap((query) => this.searchMovies2(query)), shareReplay(1))
-  private popularMovies$ = this.popularMoviesQuerySub.pipe(tap((query) => {console.log('popularMoviesQuerySub emits: ', query)}), switchMap((query) => this.getPopularMovies(query)), shareReplay(1))
+  private popularMovies$ = this.popularMoviesQuerySub.pipe(switchMap((query) => this.getPopularMovies(query)), shareReplay(1))
 
   movies$: Observable<MovieListItem[]> = this.userInteractionsSub.pipe(switchMap(
     (i) => {
       if(!i) return of([])
-      console.log('userInteractionsSub, this.query: ', this.queryString || '<empty>')
       if(this.queryString){
-        console.log('userInteractionsSub, searchedMovies')
-        this.searchedMoviesPage = i === UserInteraction.Search ? 1 : this.searchedMoviesPage + 1
-        this.searchedMoviesQuerySub.next(this.queryString);
+        const shouldFetchMovies = i !== UserInteraction.Init
+        if(shouldFetchMovies){
+          this.searchedMoviesPage = i === UserInteraction.Search ? 1 : this.searchedMoviesPage + 1
+          this.searchedMoviesQuerySub.next(this.queryString);
+        }
         return this.searchedMovies$
       }else{
-        console.log('userInteractionsSub, popularMovies')
-        /*
-        TODO here update popular movies will be exec whenever navigating back to the Search page which is non necessary
-        */
-        if(i === UserInteraction.LoadMore || ((i=== UserInteraction.Search && this.queryString === '' && this.popularMoviesPage === 0))){
+       const shouldFetchMovies = (i !== UserInteraction.Init && i === UserInteraction.LoadMore)
+       if(shouldFetchMovies){
           this.popularMoviesPage++
           this.popularMoviesQuerySub.next(this.queryString) // update popular movies / popularMovies$ emits value
         }
@@ -56,6 +47,10 @@ export class MoviesService {
       }
     })
   )
+  
+  init(){
+    this.userInteractionsSub.next(UserInteraction.Init)
+  }
 
   searchMovies(query: string){
     this.updateMovies(UserInteraction.Search, query)
@@ -66,30 +61,8 @@ export class MoviesService {
   }
 
   private updateMovies(interaction: UserInteraction, query: string){
-    console.log('updateMovies, interaction: ', interaction, '; query: ', query, '<empty>')
-    this.currentPage = interaction === UserInteraction.Search ? 1 : this.currentPage + 1
-
     this.queryString = query
-    // this.querySub.next(query)
-    /*if(interaction === UserInteraction.Search){
-      this.searchedMoviesQuerySub.next(query)
-    }else{
-      this.popularMoviesQuerySub.next(query)
-    }*/
-
     this.userInteractionsSub.next(interaction)
-  }
-
-  private getMovies(query: string): Observable<MovieListItem[]>{
-    return this.moviesApiService.getMovies({query, page: this.currentPage}).pipe(
-      tap(() => { this.isLoading = true }),
-      map((data: PaginatedResult<MovieListItem>) => {
-        const isLoadMore = this.currentPage > 1
-        this.loadedMovies = isLoadMore ? this.loadedMovies.concat(data.results) : data.results
-        return this.loadedMovies
-      }), 
-      finalize(() => {this.isLoading = false })
-    );
   }
 
   private searchedMovies: MovieListItem[] = []
